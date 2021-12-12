066ca9c1c6accdd82bb46f73758d75536b4abce6
Wellyton <53409171+WellytonSdJ@users.noreply.github.com>
2021-12-12 00:16:01 +0000

    Atualizadas cores, adicionada ação no menu hamburguer, iniciado outras seções.

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-var aki = document.getElementById('botao')
+// header scrolling effect
+$(window).on('scroll', function(){
+	if($(window).scrollTop()){
+      $('header').addClass('nav-show');
+		  
+	} 
+	else{
+		$('header').removeClass('nav-show');
+	}
+	   
+})
 
-aki = addEventListener('click', eta)
+//hamburger
+const navSlide = () => {
+	 const hamburger = document.querySelector(".hamburger");
+	 const navbar = document.querySelector(".nav-bar");
+	 const navLinks = document.querySelectorAll(".nav-bar li");
 
-function eta() {
-    console.log('foi')
-}
+     hamburger.onclick = () => {
+		
+	 navbar.classList.toggle("nav-active");
+		 
+      //Animation links
+	 navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+		   }
+		});
+	  //hamburger animation
+	 hamburger.classList.toggle("toggle");
+    }
+	 
+	}
+
+window.onload = () => navSlide();
